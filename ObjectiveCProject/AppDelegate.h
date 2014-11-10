37d76a57e0e8481091a8c637c2b4ea965ce79c09
Brian Mendez <brianmendez@Brians-MacBook-Pro.local>
2014-11-10 09:18:28 -0800

    Initial Commit

---
+++ ObjectiveCProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveCProject
+//
+//  Created by Brian Mendez on 11/10/14.
+//  Copyright (c) 2014 Brian Mendez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveCProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveCProject
+//
+//  Created by Brian Mendez on 11/10/14.
+//  Copyright (c) 2014 Brian Mendez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
